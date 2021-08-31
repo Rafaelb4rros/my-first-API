c3b477ce398c206538e6c75feaad70e2f631a5c1
Rafael Barros <rafaelnogueirabarr@gmail.com>
2021-08-31 08:40:33 -0300

    a

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 import express from "express";
-import dotenv from "dotenv";
 import cors from "cors";
 import Router from "./src/server/routes/serverRoutes.js"
 import errorHandler from "./src/server/errors/ErrorHandler.js";
@@ -7,7 +6,6 @@ import convertAllrequeststoJson from "./src/Middlewares/middlewaretest.js";
 import userRouter from "./src/server/db/tableusers/tableusersroutes/tableusersRoute.js";
 const app = express();
 
-dotenv.config();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({
