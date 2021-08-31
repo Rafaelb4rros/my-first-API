688e41381a9f465b3bc2c62407fb36fd49fcadc0
Rafael Barros <rafaelnogueirabarr@gmail.com>
2021-08-31 10:08:03 -0300

    tentando arrumar .-.

--- src/server/db/tableusers/usersQuerys/test.js
+++ src/server/db/tableusers/usersQuerys/test.js
@@ -1,16 +1,14 @@
 import express from "express";
-import userQuery from "./insertuser";
+import userQuery from "./insertuser.js";
 import bcrypt from "bcrypt";
 const testdele = express.Router();
 
 
 testdele.post('/', async(req, res) =>{
     try { 
-        let Id = req.body.id;
         let Email = req.body.email;
         let Login = req.body.login
         let Senha = req.body.senha;
-        Id = await bcrypt.hash(Id);
         Email = await bcrypt.hash(Email);
         Login = Login;
         Senha = await bcrypt.hash(Senha);
@@ -20,6 +18,8 @@ testdele.post('/', async(req, res) =>{
         password: Senha
     })
     const test = await userQuery.postadmin(data);
+    let Id = test.id;
+    Id = await bcrypt.hash(Id);
     res.send(200).json(test) 
 } catch (err){
     console.log(err);
