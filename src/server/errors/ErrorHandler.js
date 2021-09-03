8ac0415df52aef771a0dd131e24a000dc9354cf1
Rafael Barros <rafaelnogueirabarr@gmail.com>
2021-09-03 13:12:00 -0300

    diminuindo vida util do jWt

--- src/server/errors/ErrorHandler.js
+++ src/server/errors/ErrorHandler.js
@@ -2,14 +2,17 @@ import NotFound from "./NotFount.js";
 import AlreadyExists from "./AlreadyExists.js";
 import InvalidAssignment from "./InvalidAssignment.js";
 import InvalidUser from "./InvalidUser.js";
-import { JsonWebTokenError } from "jsonwebtoken";
+import pkg from 'jsonwebtoken';
+import Sequelize from 'sequelize';
+const { ValidationError } = Sequelize;
+const { JsonWebTokenError, TokenExpiredError} = pkg;
 
 const errorHandler = (error, req, res, next) => {
     let errorStatus = 500;
     if (error instanceof NotFound || InvalidUser ) {
         errorStatus = 404;
     }
-    if (error instanceof InvalidAssignment || ValidationError || JsonWebTokenError) {
+    if (error instanceof InvalidAssignment || ValidationError || JsonWebTokenError || TokenExpiredError) {
         errorStatus = 400;
     }
     if (error instanceof AlreadyExists) {
