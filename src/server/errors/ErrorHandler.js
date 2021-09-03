eb83b0d17ac9904b1e5d6bfebc8db21993f3187e
Rafael Barros <rafaelnogueirabarr@gmail.com>
2021-09-03 13:03:39 -0300

    Filtrando mais erros

--- src/server/errors/ErrorHandler.js
+++ src/server/errors/ErrorHandler.js
@@ -2,19 +2,21 @@ import NotFound from "./NotFount.js";
 import AlreadyExists from "./AlreadyExists.js";
 import InvalidAssignment from "./InvalidAssignment.js";
 import InvalidUser from "./InvalidUser.js";
+import { ValidationError } from "sequelize/types";
+import { JsonWebTokenError } from "jsonwebtoken";
 
 const errorHandler = (error, req, res, next) => {
     let errorStatus = 500;
     if (error instanceof NotFound || InvalidUser ) {
         errorStatus = 404;
     }
-    if (error instanceof InvalidAssignment) {
+    if (error instanceof InvalidAssignment || ValidationError || JsonWebTokenError) {
         errorStatus = 400;
     }
     if (error instanceof AlreadyExists) {
         errorStatus = 406;
     }
-
+    
     res.status(errorStatus);
     res.send({
         mensagem: error.message,
